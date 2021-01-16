4fa0277ac03fcb4c365b76e7112ebd3ed2e21bbf
chevtong <chevtongwingyan@gmail.com>
2021-01-16 16:41:06 +0100

    Change text of submit btn according to the attempts

--- Starter-packs/1.Guessing-game/view.php
+++ Starter-packs/1.Guessing-game/view.php
@@ -14,13 +14,12 @@
 	<form action="index.php" method="post">
 	Guess A Number (1-10)<br>
 	<input type="number" max="10" name="inputNumber"><br>
-	<input type="submit" name="submit" value="<?php if(!empty($game->attempts) && ( $game->attempts > 0)){ 
-		echo "submit";
-	} else if ($game->attempts = 0) {
-		echo "Again";}; ?> ">
-
-	
+	<input type="submit" name="submit" value="<?php  if( $game->attempts == 0){ 
+		echo "RESTART"; 
+	} else {
+		echo "SUBMIT";} //to change the text of the input from submit to restart?>" >
 	</form>
+	
 
 	<h3>Your Choice: <?php if(isset($_POST["inputNumber"])){echo $_POST["inputNumber"];}?></h3>
 	<p> Attempts: <?php if(!empty($game->attempts)){ echo $game->attempts;}?>
